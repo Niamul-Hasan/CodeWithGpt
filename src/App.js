8f44844c68d0af75e40a54ead85457c8588652cc
Niamul Hasan Zead <zead.ece2014@gmail.com>
2023-01-31 19:01:06 +0600

    navbar is created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Home from './Components/AppBar/Home';
+import NavBar from './Components/AppBar/NavBar';
+import TopBar from './Components/AppBar/TopBar';
 import ExampleCarousel from './Components/gptCarosel';
 
 function App() {
   return (
     <div className="">
+      <TopBar />
+      <NavBar />
       <ExampleCarousel />
+      <Home />
     </div>
   );
 }
